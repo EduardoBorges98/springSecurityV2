946aef7956abe84d3b576d7596fe159419b871eb
Eduardo Borges <eduardo_borges98@hotmail.com>
2026-04-14 15:17:48 -0300

    servico de email real

--- src/main/java/med/voll/web_application/domain/usuario/DadosAlteracaoSenha.java
+++ src/main/java/med/voll/web_application/domain/usuario/DadosAlteracaoSenha.java
@@ -2,6 +2,8 @@ package med.voll.web_application.domain.usuario;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record DadosAlteracaoSenha(@NotBlank String senhaAtual, @NotBlank String novaSenha, @NotBlank String novaSenhaConfirmacao) {
+public record DadosAlteracaoSenha(@NotBlank String senhaAtual,
+                                  @NotBlank String novaSenha,
+                                  @NotBlank String novaSenhaConfirmacao) {
 
 }
